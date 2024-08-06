92373b46d17a09c4e491096a9cd15051a84eb32f
psmurillom <psmurillom@indracompany.com>
2024-08-06 18:09:34 -0500

    Revisión de master pass

--- scripts/deploy-01-deploy.sh
+++ scripts/deploy-01-deploy.sh
@@ -4,6 +4,9 @@ sed -i "s|image: testjenkins:.*|image: ${DOCKER_REGISTRY}/${IMAGE_FULL_NAME}|" d
 
 cat deployment.yaml
 
+echo "Se configura JASYPT_ENCRYPTOR_PASSWORD"
+echo $MASTER_PASS;
+
 kubectl set env deployment/testjenkins JASYPT_ENCRYPTOR_PASSWORD=$MASTER_PASS
 
 kubectl apply -f deployment.yaml
