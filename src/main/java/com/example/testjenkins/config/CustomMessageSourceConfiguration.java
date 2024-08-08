3befe9190731c692d68f7d99df01671a274e8d98
psmurillom <psmurillom@indracompany.com>
2024-08-08 14:09:53 -0500

    Ajuste mensajes sobreescritos

--- src/main/java/com/example/testjenkins/config/CustomMessageSourceConfiguration.java
+++ src/main/java/com/example/testjenkins/config/CustomMessageSourceConfiguration.java
@@ -25,7 +25,7 @@ public class CustomMessageSourceConfiguration {
 			StringBuilder base = new StringBuilder("");
 			base.append(env.getProperty(AppConstants.PROPERTIES_PATH));
 			base.append("/messages");
-			messageSource.setBasename(base.toString());
+			messageSource.setBasenames("classpath:messages", base.toString());
 		} else {
 			messageSource.setBasename("classpath:messages");
 		}
