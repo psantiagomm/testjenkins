e92bf98d54d512332493065fa14b597ada13d7c5
psmurillom <psmurillom@indracompany.com>
2024-08-09 11:05:57 -0500

    Revisión normalización

--- deploy/scripts/update-properties-01-configmap.sh
+++ deploy/scripts/update-properties-01-configmap.sh
@@ -6,6 +6,16 @@ APP_PASSWORD=$(sh ./deploy/scripts/encrypt.sh -m $MASTER_PASS -p $APP_PASSWORD)
 MESSAGES_PROPERTIES=$(echo "$MESSAGES_PROPERTIES" | sed '2,$ s/^/    /')
 RESILIENCE_PROPERTIES=$(sh ./deploy/scripts/normalize.sh -p $RESILIENCE_PROPERTIES)
 APPLICATION_PROPERTIES2=$(echo "${APPLICATION_PROPERTIES}" | sed 's/^/    /')
+APPLICATION_PROPERTIES3=$(sh ./deploy/scripts/normalize.sh -p $APPLICATION_PROPERTIES)
+
+echo "Original"
+echo $APPLICATION_PROPERTIES
+
+echo "Con echo y sed"
+echo $APPLICATION_PROPERTIES2
+
+echo "Normalize"
+echo $APPLICATION_PROPERTIES3
 
 # Crear el archivo configmap.yaml con los valores de los parámetros
 cat <<EOF > configmap.yaml
